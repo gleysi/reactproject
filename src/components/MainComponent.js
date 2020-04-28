31dd46a56a553931ae9ff68581b21f93935fb657
Miranda Tejada <sonia.miranda.tejada@accenture.com>
2020-04-28 15:33:30 -0500

    Intro. to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,8 +10,17 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 class Main extends Component {
 
   constructor(props) {
@@ -67,5 +76,4 @@ class Main extends Component {
     );
   }
 }
-
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
